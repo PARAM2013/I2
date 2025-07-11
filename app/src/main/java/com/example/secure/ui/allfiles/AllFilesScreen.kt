8a93526e9fe5c02aa62522b1fef3a89332a49e65
param.a <param.a@hotmail.com>
2025-07-11 12:30:45 +0530

    Fix new UI 2

--- app/src/main/java/com/example/secure/ui/allfiles/AllFilesScreen.kt
+++ app/src/main/java/com/example/secure/ui/allfiles/AllFilesScreen.kt
@@ -19,8 +19,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.secure.R
 import com.example.secure.ui.theme.ISecureTheme
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
